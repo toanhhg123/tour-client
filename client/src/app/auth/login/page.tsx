3a28e8007634d3f1fe8df63f0f27ed81a0c4e627
toanhhg123 <toanhhg123@gmail.com>
2023-10-05 15:11:52 +0700

    change btn login

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -91,7 +91,7 @@ export default function LoginAccount() {
             </Form>
           </CardContent>
           <CardFooter className="flex flex-col">
-            <Button className="w-full bg-red-600">Login</Button>
+            <Button className="w-ful">Login</Button>
           </CardFooter>
         </Card>
       </form>
