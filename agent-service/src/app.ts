d03e8594e5b763ba53ae4e48654d4e3a1b0a3c90
toanhhg123 <toanhhg123@gmail.com>
2023-10-11 14:17:41 +0700

    add middleware prom client

--- agent-service/src/app.ts
+++ agent-service/src/app.ts
@@ -1,31 +1,43 @@
 import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
+import * as client from 'prom-client'
 import connectDb from './config/connect'
 import { handleError, notFound } from './middlewares'
 import morganMiddleware from './middlewares/morganMiddleware'
 import useRouter from './routes'
-import Prometheus from 'prom-client'
 
 async function BootStrap() {
   const app = express()
 
-  const register = new Prometheus.Registry()
-  Prometheus.collectDefaultMetrics({ register })
-
-  const http_request_duration_milliseconds = new Prometheus.Histogram({
-    name: 'myapp_http_request_duration_milliseconds',
-    help: 'Duration of HTTP requests in milliseconds.',
-    labelNames: ['method', 'route', 'code'],
-    buckets: [1, 2, 3, 4, 5, 10, 25, 50, 100, 250, 500, 1000]
+  const register = new client.Registry()
+  register.setDefaultLabels({
+    app: 'agent-service'
   })
-
-  register.registerMetric(http_request_duration_milliseconds)
+  const httpRequestTimer = new client.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500, 1000]
+  })
+  client.collectDefaultMetrics({ register })
 
   app.get('/metrics', function (req, res) {
     res.setHeader('Content-Type', register.contentType)
 
     register.metrics().then((data) => res.status(200).send(data))
+  })
+
+  app.get('/tweets', async (req, res) => {
+    const start = Date.now()
+    try {
+      console.log('performent running')
+    } finally {
+      const responseTimeInMs = Date.now() - start
+      httpRequestTimer
+        .labels(req.method, req.route.path, res.statusCode.toString())
+        .observe(responseTimeInMs)
+    }
   })
 
   app.use(helmet())
