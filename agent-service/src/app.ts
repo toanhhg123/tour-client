a21ebd01e251cc2031c7546326107ac719cdff95
toanhhg123 <toanhhg123@gmail.com>
2023-10-11 10:15:27 +0700

    add promethus config and add middlare pro to agent

--- agent-service/src/app.ts
+++ agent-service/src/app.ts
@@ -5,9 +5,28 @@ import connectDb from './config/connect'
 import { handleError, notFound } from './middlewares'
 import morganMiddleware from './middlewares/morganMiddleware'
 import useRouter from './routes'
+import Prometheus from 'prom-client'
 
 async function BootStrap() {
   const app = express()
+
+  const register = new Prometheus.Registry()
+  Prometheus.collectDefaultMetrics({ register })
+
+  const http_request_duration_milliseconds = new Prometheus.Histogram({
+    name: 'myapp_http_request_duration_milliseconds',
+    help: 'Duration of HTTP requests in milliseconds.',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [1, 2, 3, 4, 5, 10, 25, 50, 100, 250, 500, 1000]
+  })
+
+  register.registerMetric(http_request_duration_milliseconds)
+
+  app.get('/metrics', function (req, res) {
+    res.setHeader('Content-Type', register.contentType)
+
+    register.metrics().then((data) => res.status(200).send(data))
+  })
 
   app.use(helmet())
   app.use(express.json())
