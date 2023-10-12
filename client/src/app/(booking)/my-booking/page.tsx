6aec330f0d9de9e7c0f6405b37c28df8085a7ec2
toanhhg123 <toanhhg123@gmail.com>
2023-10-12 21:37:33 +0700

    deploy test jenkins

--- client/src/app/(booking)/my-booking/page.tsx
+++ client/src/app/(booking)/my-booking/page.tsx
@@ -3,21 +3,11 @@ import PrivateRoute from '@/context/PrivateRouteContext'
 import { getToursThunk } from '@/features/tour/actions'
 import useDispatchAsync from '@/hooks/useDispatchAsync'
 import { useAppSelector } from '@/store/hooks'
-import { usePathname, useSearchParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 const Page = () => {
-  const { tours } = useAppSelector((state) => state.tour)
-  const { bookingByListTours } = useAppSelector((state) => state.booking)
+  const { bookings } = useAppSelector((state) => state.booking)
   const { dispatchAsyncThunk } = useDispatchAsync()
-
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  const search = searchParams.get('search') || ''
-  const pageIndex = Number(searchParams.get('pageIndex') || 1)
-
-  const { total, limit } = tours
 
   useEffect(() => {
     dispatchAsyncThunk(getToursThunk())
