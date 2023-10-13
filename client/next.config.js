7695a51d4124ed410c3bf6863f2ac42dbd6da752
toanhhg123 <toanhhg123@gmail.com>
2023-10-13 10:18:56 +0700

    change docker file client

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   eslint: {
     dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
+  outputStandalone: true,
 }
 
 module.exports = nextConfig
